ab64af184d13d99b89bb8d14c529566952902a92
ArthurVSO <Arthurvenicio3@gmail.com>
2023-03-01 15:37:23 -0300

    primeiro modulo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int idade = 21;
+        System.out.println(idade);
     }
 }
